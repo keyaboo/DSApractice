c5dd05e7e5bfdeea156b76c977a2ee343b8ee437
christian-ramsland <weeaboochamploo@protonmail.com>
2023-03-31 21:13:36 -0400

    indexing answer for DT proving to be difficult

--- src/main/java/com/leetcode/inprogress/DailyTemperatures_739.java
+++ src/main/java/com/leetcode/inprogress/DailyTemperatures_739.java
@@ -17,27 +17,42 @@ import java.util.Stack;
  * solutions make reference to this data structure which I'm gonna explore a bit more.
  * tried with normal stack and that's obviously not working, no idea why this is a recommended
  * problem so early in adm.
+ *
+ *
  */
 public class DailyTemperatures_739 {
 
 	public static void main(String[] args) {
 		int[] temperatures = new int[] {73,74,75,71,69,72,76,73};
-		dailyTemperatures(temperatures);
+		System.out.println(useStack(temperatures));
 	}
-
-	public static void useStack(int[] temps, List<Integer> answer) {
+	// monotonically decreasing stack I think is the play here
+	public static int[] useStack(int[] temps) {
 		Stack<Integer> stck = new Stack<>();
-		Integer highest = temps[0];
-		for (int i = 0; i < temps.length; i++) {
-			if (stck.isEmpty() || temps[i] < highest) {
-				stck.push(temps[i]);
-			} else if (temps[i] > highest) {
-				highest = temps[i];
-				while (!stck.isEmpty()) {
+		ArrayList<Integer> answers = new ArrayList<>();
+		answers.add(0, 9);
+		int popCounter = 0;
 
+		for (int i = 0; i < temps.length; i++) {
+			if (stck.isEmpty() || temps[i] <= stck.peek()) {
+				stck.push(temps[i]);
+				popCounter = 0;
+			} else {
+				while (temps[i] >= stck.peek()) {
+					popCounter++;
+					answers.add(answers.size()-popCounter, popCounter);
+					stck.pop();
+					if (stck.isEmpty()) {
+						break;
+					}
 				}
+				stck.push(temps[i]);
+				System.out.println("debug");
 			}
+
 		}
+		System.out.println(answers);
+		return answers.stream().mapToInt(i -> i).toArray();
 	}
 
 
@@ -45,60 +60,7 @@ public class DailyTemperatures_739 {
 	/*
 	this is garbage the
 	 */
-	public static int[] dailyTemperatures2(int[] temps) {
-		if (temps.length == 1) {
-			return new int[]{0};
-		}
-		int start = 0;
-		int stop = start + 1;
-		ArrayList<Integer> output = new ArrayList<>();
-		while (stop < temps.length) {
-			if (stop == temps.length - 1) {
-
-			}
-
-			if (temps[stop] > temps[start]) {
-				output.add(start,start-stop);
-				if (start - stop > 2) {
-					output.add(stop-1,1);
-					int left = start + 1;
-					int right = stop - 1;
-					while (right > start) {
-
-					}
-				}
-
-			} else {
-				stop++;
-			}
-		}
-		return new int[]{0};
-	}
-
 	
-	private static void dailyTemperatures(int[] temperatures) {
-		int[] answer = new int[temperatures.length - 1];
-		ArrayList<Integer> maxPositions = new ArrayList<Integer>();
-		int temp = temperatures[0];
-		for (int i = 0; i < temperatures.length; i++) {
-			if (temperatures[i] > temp) {
-				maxPositions.add(i);
-			}
-		}
-		determinePositions(0, temperatures, maxPositions, answer);
-	}
-	// [1,1,4,2,1,1,0,0]
-	private static void determinePositions(int offset, int[] temperatures, ArrayList<Integer> maxPositions, int[] answer) {
-		for (int i = 0; i < maxPositions.size(); i++) {
-			int x = maxPositions.get(i);
-			if (i == 0) {
-				answer[offset] = maxPositions.get(i + 1) - maxPositions.get(i);
-			} else if (i > 1 && i < maxPositions.size() - 1) {
-				offset = maxPositions.get(i);
-				answer[offset] = maxPositions.get(i + 1) - maxPositions.get(i);
-			}
-		}
-	}
 	
 	
 
