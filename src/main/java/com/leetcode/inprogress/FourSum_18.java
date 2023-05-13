06578156c08f53f741d1b53c24ff0a3b95672d6e
christian-ramsland <weeaboochamploo@protonmail.com>
2023-05-12 23:32:56 -0400

    four sum slept on

--- src/main/java/com/leetcode/inprogress/FourSum_18.java
+++ src/main/java/com/leetcode/inprogress/FourSum_18.java
@@ -3,7 +3,7 @@ package com.leetcode.inprogress;
 import java.util.*;
 
 /**
- * brute force worse than O(n^2).
+ * do 3 sum minus target and put indices in a set as hashmap val with their sum as key
  */
 public class FourSum_18 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
