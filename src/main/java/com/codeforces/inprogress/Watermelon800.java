2e0b872c118c093c35599a1787233e3fe8608012
christian-ramsland <weeaboochamploo@protonmail.com>
2023-06-17 00:54:53 -0400

    trying codeforces

---
+++ src/main/java/com/codeforces/inprogress/Watermelon800.java
@@ -0,0 +1,20 @@
+package com.codeforces.inprogress;
+
+
+import java.util.Scanner;
+
+public class Watermelon800 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int weight = sc.nextInt();
+        isDivisibleEvenly(weight);
+    }
+
+    private static void isDivisibleEvenly(int weight) {
+        if (weight >= 4 && weight % 2 == 0)
+            System.out.println("YES");
+        else {
+            System.out.println("NO");
+        }
+    }
+}
