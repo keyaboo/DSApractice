290693b3bd02d6a6bf41e5c418e27a0453c222f4
christian-ramsland <chris.ramsland@gmail.com>
2024-12-04 16:43:23 -0500

    treeset sorting problems and subsequence daily

--- src/main/java/com/codeforces/contests/Div2Round172/Recommendations/Main.java
+++ src/main/java/com/codeforces/contests/Div2Round172/Recommendations/Main.java
@@ -2,6 +2,16 @@ package com.codeforces.contests.Div2Round172.Recommendations;
 
 import java.util.*;
 
+/*
+Looking at editorial: yes next largest interval is what we're trying to find. Sorting criteria is the
+tracks[0] term not the width. If l_j, r_j is the interval we want to find predictions for, then we're
+going to do a binary search in [L, l_j) to find an l_i for which r_i > r_j. Sort segments by l_i, using
+r_i as a tiebreaker. The solution recommends a freqs map rather than one that holds indices of the
+original tracks. A question, since I am not terribly familiar with TreeSets, is what happens if we haven't
+seen the ideal interval yet to call ceiling? That's already solved by having the intervals in ascending
+left interval values added to the treeset already. The flip is clever to repeat the same process going
+in the other direction.
+ */
 public class Main {
     public static void main(String[] args) {
         try {
@@ -16,12 +26,11 @@ public class Main {
                         tracks[j][0] = sc.nextLong();
                         tracks[j][1] = sc.nextLong();
                     }
-                    List<Long> res = fnName(tracks);
+                    List<Long> res = getStrongRecommendations(tracks);
                     for (int k = 0; k < res.size(); k++) {
-                        System.out.println(res);
+                        System.out.println(res.get(k));
                     }
                 }
-
 
             }
         } catch (Exception e) {
@@ -32,31 +41,95 @@ public class Main {
 
     }
 
-    /*
-    my thought is to do a min heap based on the first interval value. But from there what happens?
-    you'd have overlapping intervals,and in order to determine the number of strongly recommended
-    tracks you'd need to find the next largest interval over that range and count the difference.
-    So sort based on interval width rather than interval start? binary search for it but what kind
-    of data structure could preserve the interval history. you could start from the end but it'd take
-    forever. or you could have one long array and fill it in with values and some sort of marker
-    as to whether it was a start or end. bit manipulation?
-     */
-    private static List<Long> fnName(Long[][] segments) {
-        List<Long> tracks = new ArrayList<>();
-        HashMap<Long, List<Integer>> indexByValue = new HashMap<>();
-        for (int i = 0; i < segments.length; i++) {
-            long start = segments[i][0];
-            long end = segments[i][1];
-            indexByValue.put(start, indexByValue.computeIfAbsent(start, k -> new ArrayList<>())).add(i);
-            indexByValue.put(end,indexByValue.computeIfAbsent(start, k -> new ArrayList<>())).add(-i);
+
+    public static List<Long> getStrongRecommendations(Long[][] tracks) {
+        List<Seg> segments = new ArrayList<>();
+        for (int i = 0; i < tracks.length; i++) {
+            Seg seg = new Seg(tracks[i][0], tracks[i][1]);
+            segments.add(seg);
         }
+        List<Long> res = new ArrayList<>(Collections.nCopies(tracks.length, 0L));
+        for (int k = 0; k < 2; k++) {
+            List<Integer> indices = new ArrayList<>();
+            for (int i = 0; i < tracks.length; i++) {
+                indices.add(i);
+            }
+
+            Collections.sort(indices, (i, j) ->
+                segments.get(i).compareTo(segments.get(j))
+            );
+            TreeSet<Long> bounds = new TreeSet<>();
+            for (int i : indices) {
+                // System.out.println(segments.get(i));
+                Long smallestRight = bounds.ceiling(segments.get(i).end);
+                if (smallestRight != null) {
+                    res.set(i, res.get(i) + smallestRight - segments.get(i).end);
+                }
+                bounds.add(segments.get(i).end);
+            }
+            for (int i = 0; i < segments.size(); i++) { // this is flipping everything and doing right to left sweep.
+                segments.get(i).flipSeg();
+            }
+
+        }
+        HashMap<String, Integer> counts = new HashMap<>();
+        for (Seg s : segments) {
+            String key = s.start + "_" + s.end;
+            counts.put(key, counts.getOrDefault(key, 0) + 1);
+        }
+        for (int i = 0; i < segments.size(); i++) {
+            String key = segments.get(i).start + "_" + segments.get(i).end;
+            if (counts.get(key) > 1) {
+                res.set(i, 0L);
+            }
+        }
+        return res;
+    }
+
+    private static class Seg implements Comparable<Seg> {
+        public long start;
+        public long end;
+
+        Seg(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public void flipSeg() {
+            this.start = (-1) * this.start;
+            this.end = (-1) * this.end;
+            long temp = this.start;
+            this.start = this.end;
+            this.end = temp;
+        }
+        @Override
+        public int compareTo(Seg other) {
+            if (start != other.start) {
+                return Long.compare(this.start, other.start); // returns smaller of this.start & other.start
+            }
+            return Long.compare(other.end, this.end); // returns larger of this.end and other.end
+            // prioritize longest
+        }
+        @Override
+        public String toString() {
+            return "(" + start + ", " + end + ")";
+        }
+
         /*
-        while (!pq.isEmpty()) {
-            Long[] curr = pq.poll();
-
+        nice to know how to do this but just making it a string is fine too.
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Seg seg = (Seg) o;
+            return start == seg.start && end == seg.end;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, end);
         }
 
-         */
-        return null;
+
     }
 }
--- src/main/java/com/leetcode/inprogress/MakeStringSubsequenceCyclicIncrements_2825.java
+++ src/main/java/com/leetcode/inprogress/MakeStringSubsequenceCyclicIncrements_2825.java
@@ -4,26 +4,42 @@ public class MakeStringSubsequenceCyclicIncrements_2825 {
     /*
     subsequence is like, ac is a subsequence of abc because the order is preserved but the string is obviously
     smaller and different.
+
+    This was incredibly sloppy not understanding of the problem at the beginning. str1 and str2 are definite,
+    but the code for retrieving longest/shortest is in a void helper function.
+
+    regular subsequence equivalence could probably be like, for abc and ac you'd have a counter for str2
+    and not increment it if str1.charat(i) != str2.charAt(idx), and see if it gets to str2.length(), again
+    made a mistake wanting to add i+idx to str1. the situation for cyclic increments, since you have two
+    candidates and uncertain whether to increment that index. could have three separate counters, a +1,
+    a 0. fast & slow pointer problem, except what would the rule be for incrementing them.
+
+    the first hint is to consider the incremented elements separately, they'd both have to get incremented
+    i'm guessing because it doesn't matter which one "starts" our overlap. I forgot the constraint on index
+    but that's more a failing of my is-subsequence 392 napkin understanding.
      */
     public boolean canMakeSubsequence(String str1, String str2) {
-        String temp = str1;
-        str1 = (str1.length() >= str2.length()) ? str1 : str2;
-        str2 = (str2.length() > str1.length()) ? str2 : temp; // I thought to flip the order of this but no.
-        // System.out.println((char) findModuloCharacter('z', 4) + 'a');
-        // int ascii = findModuloCharacter('z', 4) + 'a';
-        for (int i = 0; i < str1.length(); i++) {
-            int diff = Math.abs(str1.charAt(0) - str2.charAt(0));
-            // diff = (str1.charAt(0) > str2.charAt(0)) ?
+        int idx = 0;
+        for (int i = 0; i < str1.length() && idx < str2.length(); i++) {
+            int ogAscii = findModuloCharacter(str1.charAt(i), 0);
+            int opAscii = findModuloCharacter(str1.charAt(i), 1);
+            int str2Ascii = findModuloCharacter(str2.charAt(idx), 0);
+            if ((opAscii == str2Ascii) || (ogAscii == str2Ascii)) {
+                idx++;
+            }
         }
-        return false;
+        return idx == str2.length();
     }
+
 
     private static int findModuloCharacter(char c, int i) {
         return (((c - 'a') + i) % 26);
     }
 
-    public static void main(String[] args) {
-        MakeStringSubsequenceCyclicIncrements_2825 object = new MakeStringSubsequenceCyclicIncrements_2825();
-        object.canMakeSubsequence("", "");
+    public void assignLongest(String str1, String str2) {
+        String temp = str1;
+        str1 = (str1.length() >= str2.length()) ? str1 : str2;
+        str2 = (str2.length() > str1.length()) ? str2 : temp; // I thought to flip the order of this but no.
     }
+
 }
